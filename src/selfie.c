fad137cc7369c056df3a7474aacb9454ded2ced1
Laurent Nguyen <laurent.nguyen@cea.fr>
2020-05-26 09:53:35 +0200

    selfie: make global input and output variables

--- src/selfie.c
+++ src/selfie.c
@@ -30,6 +30,9 @@
 #include "selfie_tools.h"
 #include "selfie_plugins.h"
 
+params_in selfie_input;
+params_out selfie_output;
+
 /// \fn      __attribute__((constructor)) void selfie_init(void)
 /// \brief   First function to be called by program
 ///
@@ -40,8 +43,6 @@
 __attribute__((constructor)) void selfie_init(void)
 {
   int i = 0;
-  params_in input;
-  params_out output;
 #ifdef HAVE_DEBUG
   PINFO("");
 #endif
@@ -54,31 +55,27 @@ __attribute__((constructor)) void selfie_init(void)
   }
 
   // Initialize params
-  selfie_alloc_params_in(&input);
-  selfie_alloc_params_out(&output);
+  selfie_alloc_params_in(&selfie_input);
+  selfie_alloc_params_out(&selfie_output);
 
   // Read configuration file
-  selfie_read_config_file(&input);
+  selfie_read_config_file(&selfie_input);
 
   // Read runtime environment variables
-  selfie_read_env_vars(&input);
+  selfie_read_env_vars(&selfie_input);
 
   // Check exclude commands
-  selfie_check_exclude(&input);
+  selfie_check_exclude(&selfie_input);
 
   // Initialize plugins
   i = 0;
   while (selfie_plugins[i] != NULL)
   {
-    (selfie_plugins[i]->init)(&input, &output);
+    (selfie_plugins[i]->init)(&selfie_input, &selfie_output);
     i++;
   }
 
   // fprintf(stderr,"Debug: %s\n",input.cmdline);
-
-  // Release params
-  selfie_free_params_in(&input);
-  selfie_free_params_out(&output);
 }
 
 /// \fn      __attribute__((destructor)) void selfie_finalize(void)
@@ -91,21 +88,17 @@ __attribute__((constructor)) void selfie_init(void)
 __attribute__((destructor)) void selfie_finalize(void)
 {
   int i = 0;
-  params_in input;
-  params_out output;
+  // params_in input;
+  // params_out output;
   char *current_locale = NULL;
 #ifdef HAVE_DEBUG
   PINFO("");
 #endif
-  // Initialize params
-  selfie_alloc_params_in(&input);
-  selfie_alloc_params_out(&output);
+  // // Read configuration file
+  // selfie_read_config_file(&input);
 
-  // Read configuration file
-  selfie_read_config_file(&input);
-
-  // Read runtime environment variables
-  selfie_read_env_vars(&input);
+  // // Read runtime environment variables
+  // selfie_read_env_vars(&input);
 
   // Get current locale from code
   current_locale = strdup(setlocale(LC_NUMERIC, ""));
@@ -117,21 +110,21 @@ __attribute__((destructor)) void selfie_finalize(void)
   i = 0;
   while (selfie_plugins[i] != NULL)
   {
-    (selfie_plugins[i]->finalize)(&input, &output);
+    (selfie_plugins[i]->finalize)(&selfie_input, &selfie_output);
     i++;
   }
 
   // Write log
-  selfie_write_log(&input, &output);
+  selfie_write_log(&selfie_input, &selfie_output);
 
   // Write output
-  selfie_write_output(&input, &output);
+  selfie_write_output(&selfie_input, &selfie_output);
 
   // Set locale to original locale
   setlocale(LC_NUMERIC, current_locale);
   free(current_locale);
 
   // Release params
-  selfie_free_params_in(&input);
-  selfie_free_params_out(&output);
+  selfie_free_params_in(&selfie_input);
+  selfie_free_params_out(&selfie_output);
 }
